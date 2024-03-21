06a3e4bd48ded9df2018b7cf41c3984126c1276b
ajithaju634 <159872661+ajithaju634@users.noreply.github.com>
2024-03-21 16:31:06 +0530

    21/3/2024

---
+++ logic/bin/logic.dart
@@ -0,0 +1,12 @@
+import 'package:logic/logic.dart' as logic;
+
+void main() {
+  String hystack = "sadbutsad";
+  String needle = "sadd";
+
+  if (hystack.contains(needle)) {
+    print("0");
+  } else {
+    print("-1");
+  }
+}
